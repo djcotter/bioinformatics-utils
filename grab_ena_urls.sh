d4c46c0f7f5578d1b49960a31ca597e4c02988d6
djcotter <dcotter1@stanford.edu>
2024-07-30 11:07:45 -0700

    add mini bash script to grab ena urls

---
+++ grab_ena_urls.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This script takes in a list of SRA or ENA accessions and uses ffq 
+# to write a list of paths to the fastq files to stdout.
+
+# The script requires the ffq tool to be installed and in the PATH.
+
+# get the list of accessions
+accessions=$1
+
+# loop over the accessions and get the fastq files
+while read acc; do
+    ffq --ftp $acc | grep -Po  '(?<=url": ") ftp://.*(?=")'
+done < $accessions
+
