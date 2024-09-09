078276b8843572ce60b0e1c9a463f6794bcae828
djcotter <dcotter1@stanford.edu>
2024-09-09 14:54:14 -0700

    fix bug with output file

--- extract_embeddings_from_torch_objects.py
+++ extract_embeddings_from_torch_objects.py
@@ -48,8 +48,7 @@ def main():
         if file.endswith(".pt"):
             embeddings[name] = extract_embeddings(os.path.join(args.input_dir, file))
 
-    output_file = args.input_dir + ".tsv"
-    output_file = os.path.join(args.input_dir, output_file)
+    output_file = args.output_file
     with open(output_file, "w") as f:
         for key, value in embeddings.items():
             f.write(key + "\t" + "\t".join([str(x) for x in value]) + "\n")
