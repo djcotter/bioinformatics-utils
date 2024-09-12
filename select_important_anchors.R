eb81df92c3669aaebc32c49e8cd77540ef40a599
Daniel Cotter <dcotter1@stanford.edu>
2024-09-12 15:14:00 -0700

    add lookuptable code to anchor selection script

--- select_important_anchors.R
+++ select_important_anchors.R
@@ -12,6 +12,7 @@
 suppressPackageStartupMessages(library(data.table))
 suppressPackageStartupMessages(library(tidyverse))
 suppressPackageStartupMessages(library(optparse))
+suppressPackageStartupMessages(library(Biostrings))
 
 ## parse arguments --------
 # define command line arguments
@@ -31,7 +32,7 @@ option_list <- list(
 opt <- parse_args(OptionParser(option_list = option_list))
 
 # check that input file exists
-if (!file.exists(opt$input) | !file.exists(opt$output)) {
+if (!file.exists(opt$input) | is.null(opt$output)) {
   stop("Must provide input and output files")
 }
 if (!file.exists(opt$lookup_table)) {
@@ -40,6 +41,8 @@ if (!file.exists(opt$lookup_table)) {
 
 # create a temporary directory to store intermediate files
 temp_dir <- file.path(dirname(opt$output), "tmp/")
+cat(paste("Creating temp directory:", temp_dir))
+cat("\n")
 system(paste("mkdir -p", temp_dir))
 
 ## load the data
@@ -54,30 +57,44 @@ if (grepl(".gz$", opt$input)) {
 }
 
 # only select the first 18 columns
-print(paste("Reading in data from", opt$input, "with effect size >=", opt$effect_size))
+cat(paste("Reading in anchors from", opt$input, "with effect size >=", opt$effect_size))
+cat("\n")
 dt <- fread(cmd=load_cmd, header=TRUE, select = 1:18)
 
 ## run lookup table to filter out artifacts -----------
 # write all anchors to a file
 anchors_to_keep <- dt %>% select(anchor) %>% distinct() 
-anchor_file <- file.path(temp_dir, "all_anchors.txt")
+anchor_file <- file.path(temp_dir, "all_anchors.txt") %>% gsub("//", "/", .)
 anchors_to_keep %>% write.table(anchor_file, row.names=FALSE, col.names=FALSE, quote=FALSE)
 
+# write anchor to a fasta file as well (for lookuptable)
+anchors_fasta_file <- file.path(temp_dir, "all_anchors.fasta") %>% gsub("//", "/", .)
+anchors_dna <- DNAStringSet(anchors_to_keep$anchor)
+names(anchors_dna) <- anchors_to_keep$anchor
+writeXStringSet(anchors_dna, anchors_fasta_file)
+
 # run lookup table
-out_lookup_stats <- file.path(temp_dir, "lookup_stats.txt")
+out_lookup_stats <- file.path(temp_dir, "lookup_stats.txt") %>% gsub("//", "/", .)
 lookup_cmd <- paste0(file.path(opt$splash_bin, "lookup_table"), 
                      " query --kmer_skip 1 --truncate_paths --stats_fmt with_stats ", 
-                     anchor_file, " ", opt$lookup_table, " ", out_lookup_stats)
-print("Running lookup table")
-system(lookup_cmd)
+                     anchors_fasta_file, " ", opt$lookup_table, " ", out_lookup_stats)
+if (file.exists(out_lookup_stats)) {
+  cat("Lookup stats already in tmp directory. Delete tmp directory to force lookup table to run again...\n")
+  cat("\nFiltering anchors for artifacts...")
+} else {
+  cat("Running lookup table...\n")
+  system(lookup_cmd)
+  cat("\nFinished Querying lookup table. Filtering anchors for artifacts...")
+}
 
 # read in the lookup table stats
 lookup_stats <- fread(out_lookup_stats, header=F, col.names=c("query", "stats"))
 lookup_stats <- lookup_stats %>% mutate(anchor = anchors_to_keep$anchor)
 
 # filter out anchors that have lookup table hits to artifacts
-artifact_pattern <- ""
-anchors_to_keep <- lookup_stats %>% filter(!grepl(artifact_pattern, stats)) %>% select(anchor)
-print(anchors_to_keep)
+artifact_pattern <- "plas|illum|syn|arp|RF|JUNK|Ral|purge|P,|Univec"
+anchors_to_keep <- lookup_stats %>% filter(!grepl(artifact_pattern, query, ignore.case=T)) %>% select(anchor)
+
 ## select the most important anchors -----------
+anchors_to_keep <- anchors_to_keep %>% left_join(dt %>% select(anchor, number_nonzero_samples),by="anchor")
 
